081cc5ec5e0696291c9e826e874a2c9e18586b7f
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2026-03-08 18:52:36 +0400

    feat: Introduce AppColors constants for consistent theming

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppColors {
+  static const Color primarySeed = Color(0xFFA63A50);
+
+  static const Color brandPrimary = Color(0xFFA63A50);
+  static const Color brandPrimaryDark = Color(0xFF3A3A3A);
+
+  static const Color brandCream = Color(0xFFF9F4EE);
+  static const Color brandWhite = Color(0xFFFFFFFF);
+  static const Color brandLightGray = Color(0xFFEDEDED);
+
+  static const Color accentLavender = Color(0xFFF4D1F7);
+  static const Color accentGold = Color(0xFFF3D588);
+  static const Color accentMint = Color(0xFFC1E3CD);
+  static const Color accentRose = Color(0xFFDEAFB8);
+
+  static const Color lightPrimary = Color(0xFFA63A50);
+  static const Color lightOnPrimary = Color(0xFFFFFFFF);
+  static const Color lightPrimaryContainer = Color(0xFFFFDADE);
+  static const Color lightOnPrimaryContainer = Color(0xFF400013);
+
+  static const Color lightSecondary = Color(0xFF3A3A3A);
+  static const Color lightOnSecondary = Color(0xFFFFFFFF);
+  static const Color lightSecondaryContainer = Color(0xFFEDEDED);
+  static const Color lightOnSecondaryContainer = Color(0xFF1A1A1A);
+
+  static const Color lightTertiary = Color(0xFFF3D588);
+  static const Color lightOnTertiary = Color(0xFF3A3A3A);
+  static const Color lightTertiaryContainer = Color(0xFFFFF0C9);
+  static const Color lightOnTertiaryContainer = Color(0xFF3D2E00);
+
+  static const Color lightError = Color(0xFFBA1A1A);
+  static const Color lightOnError = Color(0xFFFFFFFF);
+  static const Color lightErrorContainer = Color(0xFFFFDAD6);
+  static const Color lightOnErrorContainer = Color(0xFF410002);
+
+  static const Color lightSurface = Color(0xFFF9F4EE);
+  static const Color lightOnSurface = Color(0xFF3A3A3A);
+  static const Color lightSurfaceVariant = Color(0xFFEDEDED);
+  static const Color lightOnSurfaceVariant = Color(0xFF524344);
+  static const Color lightOutline = Color(0xFF857373);
+  static const Color lightOutlineVariant = Color(0xFFD7C2C3);
+
+  static const Color darkPrimary = Color(0xFFFFB2BA);
+  static const Color darkOnPrimary = Color(0xFF670025);
+  static const Color darkPrimaryContainer = Color(0xFF8A2039);
+  static const Color darkOnPrimaryContainer = Color(0xFFFFDADE);
+
+  static const Color darkSecondary = Color(0xFFEDEDED);
+  static const Color darkOnSecondary = Color(0xFF1A1A1A);
+  static const Color darkSecondaryContainer = Color(0xFF3A3A3A);
+  static const Color darkOnSecondaryContainer = Color(0xFFF9F4EE);
+
+  static const Color darkTertiary = Color(0xFFF3D588);
+  static const Color darkOnTertiary = Color(0xFF3D2E00);
+  static const Color darkTertiaryContainer = Color(0xFF594400);
+  static const Color darkOnTertiaryContainer = Color(0xFFFFF0C9);
+
+  static const Color darkError = Color(0xFFFFB4AB);
+  static const Color darkOnError = Color(0xFF690005);
+  static const Color darkErrorContainer = Color(0xFF93000A);
+  static const Color darkOnErrorContainer = Color(0xFFFFDAD6);
+
+  static const Color darkSurface = Color(0xFF1A1A1A);
+  static const Color darkOnSurface = Color(0xFFF9F4EE);
+  static const Color darkSurfaceVariant = Color(0xFF3A3A3A);
+  static const Color darkOnSurfaceVariant = Color(0xFFD7C2C3);
+  static const Color darkOutline = Color(0xFF9F8C8D);
+  static const Color darkOutlineVariant = Color(0xFF524344);
+
+  static const Color success = Color(0xFFC1E3CD);
+  static const Color successDark = Color(0xFF2E7D32);
+  static const Color successLight = Color(0xFFE8F5E9);
+  static const Color onSuccess = Color(0xFF1A3D1F);
+
+  static const Color warning = Color(0xFFF3D588);
+  static const Color warningDark = Color(0xFFE6A000);
+  static const Color warningLight = Color(0xFFFFF8E1);
+  static const Color onWarning = Color(0xFF3D2E00);
+
+  static const Color info = Color(0xFFF4D1F7);
+  static const Color infoDark = Color(0xFF7B1FA2);
+  static const Color infoLight = Color(0xFFF3E5F5);
+  static const Color onInfo = Color(0xFF3D1F42);
+
+  static const Color riskLow = Color(0xFFC1E3CD);
+  static const Color riskMedium = Color(0xFFF3D588);
+  static const Color riskHigh = Color(0xFFDEAFB8);
+  static const Color riskCritical = Color(0xFFA63A50);
+
+  static const Color priorityLow = Color(0xFFC1E3CD);
+  static const Color priorityMedium = Color(0xFFF3D588);
+  static const Color priorityHigh = Color(0xFFA63A50);
+
+  static const Color online = Color(0xFFC1E3CD);
+  static const Color offline = Color(0xFF9E9E9E);
+
+  static const Color statusCompleted = Color(0xFFC1E3CD);
+  static const Color statusPending = Color(0xFFF3D588);
+  static const Color statusInProgress = Color(0xFFF4D1F7);
+
+  static const Color upvoteActive = Color(0xFFA63A50);
+  static const Color downvoteActive = Color(0xFF3A3A3A);
+  static const Color votePositive = Color(0xFFC1E3CD);
+  static const Color voteNegative = Color(0xFFDEAFB8);
+
+  static const Color commentDepth1 = Color(0xFFA63A50);
+  static const Color commentDepth2 = Color(0xFFF3D588);
+  static const Color commentDepth3 = Color(0xFFC1E3CD);
+  static const Color commentDepth4 = Color(0xFFF4D1F7);
+  static const Color commentDepth5 = Color(0xFFDEAFB8);
+}
