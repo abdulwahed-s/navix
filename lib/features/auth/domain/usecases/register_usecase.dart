7ec6a72422d8aea17b7ed42c2093b51d7c149c9c
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2026-03-09 19:26:05 +0400

    feat: Implement RegisterUseCase for user registration functionality

---
+++ lib/features/auth/domain/usecases/register_usecase.dart
@@ -0,0 +1,28 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../../../core/error/failures.dart';
+import '../../../../core/usecases/usecase.dart';
+import '../entities/user_entity.dart';
+import '../repositories/auth_repository.dart';
+
+class RegisterUseCase implements UseCase<UserEntity, RegisterParams> {
+  final AuthRepository repository;
+
+  RegisterUseCase(this.repository);
+
+  @override
+  Future<Either<Failure, UserEntity>> call(RegisterParams params) {
+    return repository.register(email: params.email, password: params.password);
+  }
+}
+
+class RegisterParams extends Equatable {
+  final String email;
+  final String password;
+
+  const RegisterParams({required this.email, required this.password});
+
+  @override
+  List<Object?> get props => [email, password];
+}
