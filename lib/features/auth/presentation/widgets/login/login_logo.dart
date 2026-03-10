af25eabc0e65c33581fe87c9add3368a736e2fb9
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2026-03-10 20:12:40 +0400

    feat: Add LoginLogo widget for animated logo display in login UI

---
+++ lib/features/auth/presentation/widgets/login/login_logo.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/constants/app_colors.dart';
+
+class LoginLogo extends StatelessWidget {
+  final bool isDark;
+
+  final Animation<double> floatingAnimation;
+
+  const LoginLogo({
+    super.key,
+    required this.isDark,
+    required this.floatingAnimation,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: floatingAnimation,
+      builder: (context, child) {
+        return Transform.translate(
+          offset: Offset(0, floatingAnimation.value * 0.3),
+          child: Container(
+            height: 100,
+            alignment: Alignment.center,
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color:
+                        (isDark
+                                ? AppColors.darkPrimary
+                                : AppColors.brandPrimary)
+                            .withValues(alpha: 0.3),
+                    blurRadius: 30,
+                    spreadRadius: 5,
+                  ),
+                ],
+              ),
+              child: Image.asset(
+                isDark
+                    ? 'assets/images/logoWhite.png'
+                    : 'assets/images/logoBlack.png',
+                height: 80,
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
