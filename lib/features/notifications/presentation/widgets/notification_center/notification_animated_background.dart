ac258b747642f8c4c2400e0a03744e2538a28b08
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2026-03-19 01:16:23 +0400

    feat: Add NotificationAnimatedBackground widget for dynamic background animations in the notification center

---
+++ lib/features/notifications/presentation/widgets/notification_center/notification_animated_background.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/constants/app_colors.dart';
+
+class NotificationAnimatedBackground extends StatelessWidget {
+  final bool isDark;
+
+  final Animation<double> floatingAnimation;
+
+  const NotificationAnimatedBackground({
+    super.key,
+    required this.isDark,
+    required this.floatingAnimation,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: floatingAnimation,
+      builder: (context, child) {
+        return Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: isDark
+                  ? [
+                      AppColors.darkSurface,
+                      AppColors.darkPrimaryContainer.withValues(alpha: 0.2),
+                      AppColors.darkSurface,
+                    ]
+                  : [
+                      AppColors.brandCream,
+                      AppColors.accentLavender.withValues(alpha: 0.2),
+                      AppColors.brandCream,
+                    ],
+              stops: const [0.0, 0.5, 1.0],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
