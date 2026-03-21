cde1458bc12d6d9ef1e86c09a8ea1d7c10e5fde3
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2026-03-21 04:17:56 +0400

    feat: Implement GetProfileUseCase for retrieving user profiles, utilizing the ProfileRepository to fetch profile data based on user ID

---
+++ lib/features/profile/domain/usecases/get_profile_usecase.dart
@@ -0,0 +1,27 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../../../core/error/failures.dart';
+import '../../../../core/usecases/usecase.dart';
+import '../entities/profile_entity.dart';
+import '../repositories/profile_repository.dart';
+
+class GetProfileUseCase implements UseCase<ProfileEntity?, GetProfileParams> {
+  final ProfileRepository repository;
+
+  GetProfileUseCase(this.repository);
+
+  @override
+  Future<Either<Failure, ProfileEntity?>> call(GetProfileParams params) {
+    return repository.getProfile(params.userId);
+  }
+}
+
+class GetProfileParams extends Equatable {
+  final String userId;
+
+  const GetProfileParams({required this.userId});
+
+  @override
+  List<Object?> get props => [userId];
+}
