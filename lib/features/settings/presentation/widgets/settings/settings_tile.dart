9127d018c96751f8223c2112feae3ebeb587597f
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2026-04-07 04:20:14 +0400

    feat: Add SettingsTile widget for customizable settings options in settings screen

---
+++ lib/features/settings/presentation/widgets/settings/settings_tile.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class SettingsTile extends StatelessWidget {
+  final IconData icon;
+  final Color iconColor;
+  final String title;
+  final Color? titleColor;
+  final Widget? trailing;
+  final VoidCallback? onTap;
+
+  const SettingsTile({
+    super.key,
+    required this.icon,
+    required this.iconColor,
+    required this.title,
+    this.titleColor,
+    this.trailing,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return ListTile(
+      leading: Container(
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: iconColor.withValues(alpha: 0.15),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Icon(icon, color: iconColor, size: 20),
+      ),
+      title: Text(
+        title,
+        style: theme.textTheme.bodyLarge?.copyWith(
+          color: titleColor,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+      trailing: trailing,
+      onTap: onTap,
+    );
+  }
+}
