12abe0063ed3297bd5b026bd1034f12da0894924
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2026-06-14 18:16:30 +0400

    fix: Update widget test to use NavixApp instead of MyApp for accurate testing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:navix/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const NavixApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
